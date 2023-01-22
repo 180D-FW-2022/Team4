53179395c2659a0fff6b5e8a3c0b34426060bd8b
ryantien3 <ryantien3226@gmail.com>
2023-01-22 05:40:52 +0000

    add solenoid.py

---
+++ solenoid.py
@@ -0,0 +1,23 @@
+#https://images.theengineeringprojects.com/image/main/2021/03/raspberry-pi-zero-5.png
+#above link is the pinout for the Raspberry Pi Zero
+
+# The solenoid script. This script will activate the solenoid for a second and turn it off after
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+# GPIO Pin where solenoid control circuit is connected.
+solenoid_pin = 8
+
+# Define the Pin numbering type and define Servo Pin as output pin.
+#GPIO.BOARD makes the pin number correspond to the actual number on the pinout
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(solenoid_pin, GPIO.OUT)
+
+# Activate the solenoid for a second.
+GPIO.output(8, GPIO.HIGH)
+sleep(1)
+GPIO.output(8, GPIO.LOW)
+
+GPIO.cleanup()
+
